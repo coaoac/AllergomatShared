f1041a84d187825df3d121fffdfd2f865396231a
Amine Chaouki <admin@allergomat.se>
2025-03-10 22:55:50 +0100

    fix

--- Sources/AllergomatShared/Models/Picture.swift
+++ Sources/AllergomatShared/Models/Picture.swift
@@ -17,7 +17,7 @@ public struct Picture: Codable, Sendable {
     public var product: Product.ID
     public var updated: Date
 
-    public enum DataType: String, Codable {
+    public enum DataType: String, Codable,  Sendable {
         case jpeg = "image/jpeg"
         case png = "image/png"
         case gif = "image/gif"
