9eb5404261359968afd06a93f9f10414496ae7eb
Amine <admin@allergomat.se>
2025-04-05 13:49:58 +0200

    added language to user

--- Sources/AllergomatShared/Models/User.swift
+++ Sources/AllergomatShared/Models/User.swift
@@ -21,9 +21,14 @@ public struct User: Codable, Sendable {
     public var allergies: [Allergy.ID]?
     public var products: [Product.ID]?
     public var ingredients: [Keyword.ID]?
+    public var language: Language?
     public let joined: Date
-    
-    public init(_id: ID, name: String? = nil, email: String? = nil, realUserStatus: Int, role: Role, experience: Int, appleID: ID? = nil, allergies: [Allergy.ID]? = nil, products: [Product.ID]? = nil, ingredients: [Keyword.ID]? = nil, joined: Date) {
+
+    public init(
+        _id: ID, name: String? = nil, email: String? = nil, realUserStatus: Int, role: Role,
+        experience: Int, appleID: ID? = nil, allergies: [Allergy.ID]? = nil,
+        products: [Product.ID]? = nil, ingredients: [Keyword.ID]? = nil, joined: Date
+    ) {
         self._id = _id
         self.name = name
         self.email = email
@@ -38,17 +43,18 @@ public struct User: Codable, Sendable {
     }
 
     public func updated(from userInfo: BasicUserInfo) -> User {
-        User(_id: _id,
-             name: userInfo.name,
-             email: userInfo.email,
-             realUserStatus: userInfo.realUserStatus,
-             role: userInfo.role,
-             experience: userInfo.experience,
-             appleID: appleID,
-             allergies: userInfo.allergies,
-             products: userInfo.products,
-             ingredients: userInfo.ingredients,
-             joined: joined)
+        User(
+            _id: _id,
+            name: userInfo.name,
+            email: userInfo.email,
+            realUserStatus: userInfo.realUserStatus,
+            role: userInfo.role,
+            experience: userInfo.experience,
+            appleID: appleID,
+            allergies: userInfo.allergies,
+            products: userInfo.products,
+            ingredients: userInfo.ingredients,
+            joined: joined)
     }
 
     public struct Credentials: Codable, Sendable {
@@ -57,8 +63,10 @@ public struct User: Codable, Sendable {
         public let realUserStatus: Int
         public let appCode: String
         public let source: Source
-        
-        public init(name: String?, email: String?, realUserStatus: Int, appCode: String, source: Source) {
+
+        public init(
+            name: String?, email: String?, realUserStatus: Int, appCode: String, source: Source
+        ) {
             self.name = name
             self.email = email
             self.realUserStatus = realUserStatus
@@ -66,7 +74,7 @@ public struct User: Codable, Sendable {
             self.source = source
         }
     }
-    
+
     public struct Payment: Codable, Equatable, Sendable {
         public static let _cname = "Payments"
         public typealias ID = String
@@ -81,8 +89,11 @@ public struct User: Codable, Sendable {
             case monthly = "app.allergomat.plus.monthly"
             case yearly = "app.allergomat.plus.yearly"
         }
-        
-        public init(_id: ID, user: User.ID, productID: ProductID, paymentId: String, date: Date, expiry: Date) {
+
+        public init(
+            _id: ID, user: User.ID, productID: ProductID, paymentId: String, date: Date,
+            expiry: Date
+        ) {
             self._id = _id
             self.user = user
             self.productID = productID
@@ -91,55 +102,55 @@ public struct User: Codable, Sendable {
             self.expiry = expiry
         }
     }
-    
+
     public enum Role: String, Codable, Sendable {
         case normal
         case superuser
         case admin
         case owner
         case invalid
-        
+
         public func english() -> String {
             switch self {
-                case .normal: return "User"
-                case .superuser: return "Superuser"
-                case .admin: return "Admin"
-                case .owner: return "Owner"
-                case .invalid: return "Invalid"
+            case .normal: return "User"
+            case .superuser: return "Superuser"
+            case .admin: return "Admin"
+            case .owner: return "Owner"
+            case .invalid: return "Invalid"
             }
         }
-        
+
         public func swedish() -> String {
             switch self {
-                case .normal: return "Användare"
-                case .owner: return "Ägare"
-                case .invalid: return "Ogiltig"
-                case .superuser: return "Superuser"
-                case .admin: return "Admin"
+            case .normal: return "Användare"
+            case .owner: return "Ägare"
+            case .invalid: return "Ogiltig"
+            case .superuser: return "Superuser"
+            case .admin: return "Admin"
             }
         }
-        
+
         public static func allValid() -> [Role] {
             [.normal, .superuser, .admin, .owner]
         }
-        
+
         public static func allAdmin() -> [Role] {
             [.superuser, .admin, .owner]
         }
-        
+
         public func isAdmin() -> Bool {
             Self.allAdmin().contains(self)
         }
     }
-    
+
     public enum Source: String, Codable, Sendable {
         case apple
     }
-    
+
     public struct WithToken: Codable, Sendable {
         public let user: User
         public let token: Token
-        
+
         public init(user: User, token: Token) {
             self.user = user
             self.token = token
@@ -153,7 +164,7 @@ public struct User: Codable, Sendable {
         public let user: User.ID
         public let date: Date
         public var expiryDate: Date
-        
+
         public init(_id: ID, user: User.ID, date: Date, expiryDate: Date) {
             self._id = _id
             self.user = user
@@ -162,12 +173,11 @@ public struct User: Codable, Sendable {
         }
     }
 
-
     public struct Device: Codable, Sendable {
         public let model: String
         public let systemName: String
         public let systemVersion: String
-        
+
         public init(model: String, systemName: String, systemVersion: String) {
             self.model = model
             self.systemName = systemName
@@ -181,14 +191,20 @@ public struct User: Codable, Sendable {
         public var email: String?
         public let realUserStatus: Int
         public var role: User.Role
+        public var language: Language?
         public var experience: Int
         public var payments: [User.Payment]
         public var allergies: [Allergy.ID]?
         public var products: [Product.ID]?
         public var ingredients: [Keyword.ID]?
         public var token: Token.ID
-        
-        public init(_id: ID, name: String? = nil, email: String? = nil, realUserStatus: Int, role: User.Role, experience: Int, payments: [User.Payment], allergies: [Allergy.ID]? = nil, products: [Product.ID]? = nil, ingredients: [Keyword.ID]? = nil, token: Token.ID) {
+
+        public init(
+            _id: ID, name: String? = nil, email: String? = nil, realUserStatus: Int,
+            role: User.Role, experience: Int, payments: [User.Payment],
+            allergies: [Allergy.ID]? = nil, products: [Product.ID]? = nil,
+            ingredients: [Keyword.ID]? = nil, token: Token.ID, language: Language?
+        ) {
             self._id = _id
             self.name = name
             self.email = email
@@ -200,32 +216,37 @@ public struct User: Codable, Sendable {
             self.products = products
             self.ingredients = ingredients
             self.token = token
+            self.language = language
         }
 
         public static func from(user: User, payments: [Payment], token: Token) -> BasicUserInfo {
-            BasicUserInfo(_id: user._id,
-                          name: user.name,
-                          email: user.email,
-                          realUserStatus: user.realUserStatus,
-                          role: user.role,
-                          experience: user.experience,
-                          payments: payments,
-                          allergies: user.allergies,
-                          products: user.products,
-                          ingredients: user.ingredients,
-                          token: token._id)
+            BasicUserInfo(
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                realUserStatus: user.realUserStatus,
+                role: user.role,
+                experience: user.experience,
+                payments: payments,
+                allergies: user.allergies,
+                products: user.products,
+                ingredients: user.ingredients,
+                token: token._id,
+                language: user.language)
         }
 
         public static func == (lhs: BasicUserInfo, rhs: BasicUserInfo) -> Bool {
             if lhs._id == rhs._id,
-                 lhs.name == rhs.name,
-                 lhs.email == rhs.email,
-                 lhs.realUserStatus == rhs.realUserStatus,
-                 lhs.role == rhs.role,
-                 lhs.payments.sorted(by: { $0.date > $1.date}) == rhs.payments.sorted(by: { $0.date > $1.date}),
-                 lhs.allergies?.sorted(by: { $0 > $1}) == rhs.allergies?.sorted(by: { $0 > $1}),
-                 lhs.token == rhs.token,
-                 lhs.experience == rhs.experience {
+                lhs.name == rhs.name,
+                lhs.email == rhs.email,
+                lhs.realUserStatus == rhs.realUserStatus,
+                lhs.role == rhs.role,
+                lhs.payments.sorted(by: { $0.date > $1.date })
+                    == rhs.payments.sorted(by: { $0.date > $1.date }),
+                lhs.allergies?.sorted(by: { $0 > $1 }) == rhs.allergies?.sorted(by: { $0 > $1 }),
+                lhs.token == rhs.token,
+                lhs.experience == rhs.experience
+            {
                 return true
             } else {
                 return false
@@ -236,7 +257,7 @@ public struct User: Codable, Sendable {
             self.payments.contains {
                 $0.expiry >= Date()
             }
-            || role.isAdmin()
+                || role.isAdmin()
         }
 
         public func ulockedFor(experience: Int = 0) -> Bool {
@@ -244,12 +265,3 @@ public struct User: Codable, Sendable {
         }
     }
 }
-
-
-
-
-
-
-
-
-
