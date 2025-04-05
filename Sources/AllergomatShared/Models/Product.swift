2da9099f953126fd51cbc7391f00e1b1b2a0457e
Amine <admin@allergomat.se>
2025-04-05 16:02:36 +0200

    added localizations

--- Sources/AllergomatShared/Models/Product.swift
+++ Sources/AllergomatShared/Models/Product.swift
@@ -6,10 +6,11 @@ public struct Product: Codable, Equatable, Sendable {
 
     public let _id: ID  // EAN
     public var name: String
+    public var nameLocalized: LocalizedString?
     public var producer: String?
     public var origin: Country.ID?
     public var ingredients: String?
-    public var localizedIngredients: LocalizedString?
+    public var ingredientsLocalized: LocalizedString?
     public var image: Picture.ID?
     public var updated: Date
     public var dataSource: Source
@@ -17,7 +18,7 @@ public struct Product: Codable, Equatable, Sendable {
 
     public init(
         _id: ID, name: String, producer: String? = nil, origin: Country.ID? = nil,
-        ingredients: String? = nil, localizedIngredients: LocalizedString? = nil, image: Picture.ID? = nil, updated: Date, dataSource: Source,
+        ingredients: String? = nil, ingredientsLocalized: LocalizedString? = nil, image: Picture.ID? = nil, updated: Date, dataSource: Source,
         reviewed: Date? = nil
     ) {
         self._id = _id
@@ -25,7 +26,7 @@ public struct Product: Codable, Equatable, Sendable {
         self.producer = producer
         self.origin = origin
         self.ingredients = ingredients
-        self.localizedIngredients = localizedIngredients
+        self.ingredientsLocalized = ingredientsLocalized
         self.image = image
         self.updated = updated
         self.dataSource = dataSource
