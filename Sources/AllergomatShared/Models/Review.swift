6e6e510f4ed0fc4aa1f2daa09270bf09a509134f
Amine <admin@allergomat.se>
2025-04-12 15:47:11 +0200

    add

---
+++ Sources/AllergomatShared/Models/Review.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+public struct Review: Codable, Sendable {
+    public static let _cname = "Reviews"
+    public typealias ID = String
+    public let _id: ID
+    public let user: User.ID
+    public let product: Product.ID?
+    public let ingredient: Keyword.ID?
+    public let allergy: Allergy.ID?
+    public let date: Date
+    public let text: String?
+    public let approved: Bool?
+
+    public struct User_Product_Review: Codable, Sendable {
+        public let user: User.ID
+        public let product: Product.ID
+        public let review: Review?
+        public init(user: User.ID, product: Product.ID, review: Review?) {
+            self.user = user
+            self.product = product
+            self.review = review
+        }
+    }
+
+    public init(
+        _id: ID, user: User.ID, product: Product.ID?, ingredient: Keyword.ID? = nil, allergy: Allergy.ID? = nil,
+        date: Date, text: String?, approved: Bool? = nil
+    ) {
+        self._id = _id
+        self.user = user
+        self.product = product
+        self.ingredient = ingredient
+        self.allergy = allergy
+        self.date = date
+        self.text = text
+        self.approved = approved
+    }
+
+    public init(for product: Product.ID, user: User.ID, date: Date = Date(), text: String?, approved: Bool? = nil) {
+        self._id = "productReview - Product: \(product) - User: \(user)"
+        self.user = user
+        self.product = product
+        self.ingredient = nil
+        self.allergy = nil
+        self.date = date
+        self.text = text
+        self.approved = approved
+    }
+}
