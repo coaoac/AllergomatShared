ec5bc13df6ebdc5e129030cdfec28c812832c6d6
Amine <admin@allergomat.se>
2025-05-01 08:40:48 +0200

    Created Localizable Allergen model

---
+++ Sources/AllergomatShared/Models/Allergen.swift
@@ -0,0 +1,92 @@
+//
+//  Keyword.swift
+//  Allergiskan
+//
+//  Created by Chaouki, Amine on 2020-01-10.
+//  Copyright © 2020 AllergyKlubb. All rights reserved.
+//
+
+import Foundation
+import Localizer
+
+public struct Allergen: Codable, Hashable, Sendable, Identifiable {
+    public static let _cname = "Allergens"
+    public typealias ID = String
+    public let _id: ID  // code
+    public var name: LocalizedString
+    public var allergies: [Allergy.AllergyProba]
+    public var updated: Date
+    public var id: String {
+        return _id
+    }
+
+    public init(
+        _id: ID, name: LocalizedString, allergies: [Allergy.AllergyProba], updated: Date) {
+        self._id = _id
+        self.name = name
+        self.allergies = allergies
+        self.updated = updated
+    }
+
+    public struct List: Codable, Sendable {
+        public let items: [Allergen]
+
+        public init(items: [Allergen]) {
+            self.items = items
+        }
+    }
+
+    public func customizedAllergy(_ allergyProba: Allergy.AllergyProba) -> Bool {
+        self.allergies.reduce(false) { result, nextAllergyProba in
+            if nextAllergyProba.allergy == allergyProba.allergy {
+                return result || nextAllergyProba.proba != allergyProba.proba
+            } else {
+                return result || false
+            }
+        }
+    }
+
+    public func maxProba(for allergies: [Allergy.ID]) -> Proba {
+        var proba: Proba = .none
+        let relevant = self.allergies.filter { allergies.contains($0.allergy) }
+        for allergy in relevant {
+            if proba < allergy.proba {
+                proba = allergy.proba
+            }
+        }
+        return proba
+    }
+
+    public func isAllergyRelevant(_ allergies: [Allergy.ID]) -> Bool {
+        for allergy in self.allergies {
+            if allergies.contains(allergy.allergy) {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+extension Allergen: Equatable {
+    public static func == (lhs: Allergen, rhs: Allergen) -> Bool {
+        lhs.name == rhs.name
+            && lhs.allergies.reduce(true) { result, nextAllergyProba in
+                if let rAllergyProba = rhs.allergies.first(where: {
+                    $0.allergy == nextAllergyProba.allergy
+                }) {
+                    return result && rAllergyProba.proba == nextAllergyProba.proba
+                } else {
+                    return false
+                }
+            }
+            && rhs.allergies.reduce(true) { result, nextAllergyProba in
+                if let lAllergyProba = lhs.allergies.first(where: {
+                    $0.allergy == nextAllergyProba.allergy
+                }) {
+                    return result && lAllergyProba.proba == nextAllergyProba.proba
+                } else {
+                    return false
+                }
+            }
+    }
+}
